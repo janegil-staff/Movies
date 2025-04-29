3ec182344d799d3582f779dc3f9990ccf84829d9
Jan Egil Staff Øvretveit <janegilstaffovretveit@Mac.lan>
2025-04-29 11:00:45 +0200

    Fetch and display trending movies with API integration, add Results and Card components

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,25 @@
-const Home = () => {
-  return ( <div className="text-red-600"></div> );
-}
- 
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+
+const Home = async () => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  const data = await res.json();
+  console.log(data);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  
+  const results = data.results;
+
+  return (
+    <div className="text-red-600">
+      <Results results={results} />
+    </div>
+  );
+};
+
 export default Home;
